1640d0ce48c4be6cb8b582aa9993571d5abfecf5
Refayah <119056144+Refayah@users.noreply.github.com>
2025-06-06 16:15:17 +0530

    Update ReversedWords.java

--- ReversedWords.java
+++ ReversedWords.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=arr.length-1;i>=0;i--)
         {sb.append(arr[i]);
         if(i>0)
-        sb.append()=" ";
+        sb.append(" ");
      }
      return sb.toString();
     }
